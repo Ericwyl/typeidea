835eae481af8812d90de60c03d635068f58881d3
Ericwyl <weiyinlong725@163.com>
2019-05-23 00:13:47 +0800

    admin编写完成，下一步根据需求定制admin

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -33,10 +33,25 @@ class TagAdmin(admin.ModelAdmin):
         return super(TagAdmin, self).save_model(request, obj, form, change)
 
 
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    '''自定义过滤器只展示当前用户分类'''
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = [
-        'title', 'category', 'status', 'create_time', 'operator'
+        'title', 'category', 'status', 'create_time', 'operator', 'owner'
     ]
     list_filter_links = []
 
