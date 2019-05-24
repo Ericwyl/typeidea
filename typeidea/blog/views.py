86834069deba412849a5856f6deedff0ee198509
Ericwyl <weiyinlong725@163.com>
2019-05-24 17:07:26 +0800

    1

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
-
 # Create your views here.
+from django.http import HttpResponse
+from .models import Post, Tag, Category
+
+
+def post_list(request, category_id=None, tag_id=None):
+    tag = None
+    category = None
+
+    if tag_id:
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except Tag.DoesNotExist:
+            post_list=[]
+        else:
+            #访问多对多的模型：类名_set,,,,,类名小写
+            post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)
+    else:
+        post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
+        if category_id:
+            post_list = post_list.filter(category_id=category_id)
+
+
+    return render(request, 'blog/list.html', context={'post_list': 'post_list'})
+
+
+def post_detail(request, post_id):
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        post = None
+    return render(request, 'blog/detail.html', context={'post': 'post'})
+
+
+
+
--- typeidea/config/views.py
+++ typeidea/config/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def links(request):
+    return HttpResponse('links')
+
