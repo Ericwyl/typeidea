b509f722652e24a978e496091a4e40ebadf4298d
Ericwyl <weiyinlong725@163.com>
2019-06-06 16:06:04 +0800

    下一步配置rss

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,9 +1,17 @@
+from datetime import date
+
+from django.db.models import Q, F
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 # Create your views here.
 from django.http import HttpResponse
-from .models import Post, Tag, Category
+from django.views.generic import ListView, DetailView
 from config.models import SideBar
-
+from .models import Post, Category, Tag
+from django.views.generic import DetailView
+from django.views.generic import ListView
+from .models import Post
+from django.core.cache import cache
 
 def post_list(request, category_id=None, tag_id=None):
     tag = None
@@ -40,19 +48,138 @@ def post_list(request, category_id=None, tag_id=None):
     # return render(request, 'blog/list.html', context={'post_list': 'post_list'})
 
 
-def post_detail(request, post_id=None):
-    try:
-        post = Post.objects.get(id=post_id)
-    except Post.DoesNotExist:
-        post = None
+# def post_detail(request, post_id=None):
+#     try:
+#         post = Post.objects.get(id=post_id)
+#     except Post.DoesNotExist:
+#         post = None
+#
+#     context = {
+#         'post': post,
+#         'sidebars':SideBar.get_all(),
+#
+#     }
+#     context.update(Category.get_navs())
+#     return render(request, 'blog/detail.html', context=context)
 
-    context = {
-        'post': post,
-        'sidebars':SideBar.get_all(),
 
-    }
-    context.update(Category.get_navs())
-    return render(request, 'blog/detail.html', context=context)
+
+
+# class PostDetailView(DetailView):
+#     model = Post
+#     template_name = 'blog/detail.html'
+
+
+class PostListView(ListView):
+    queryset = Post.latest_posts()
+    paginate_by = 3
+    context_object_name = 'post_list'
+    template_name = 'blog/list.html'
+
+
+class CommentViewMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'sidebars': SideBar.get_all(),
+
+        })
+        context.update(Category.get_navs())
+        return context
+
+
+class IndexView(ListView):
+    queryset = Post.latest_posts()
+    paginate_by = 5
+    context_object_name = 'post_list'
+    template_name = 'blog/list.html'
+
+
+class CategoryView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs.get('category_id')
+        category = get_object_or_404(Category, pk=category_id)
+        context.update({
+            'category': category,
+
+        })
+        return context
+    def get_queryset(self):
+        '''重写queryset,根据分类过滤'''
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id)
+
+
+class TagView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag_id = self.kwargs.get('tag_id')
+        tag = get_object_or_404(Tag, pk=tag_id)
+        context.update({
+            'tag': tag
+        })
+        return context
+    def get_queryset(self):
+        '''重写queryset，根据标签过滤'''
+        queryset = super().get_queryset()
+        tag_id = self.kwargs.get('tag_id')
+        return queryset.filter(tag_id=tag_id)
+
+
+class PostDetailView(CommentViewMixin, DetailView):
+    model = Post
+    queryset = Post.latest_posts()
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+    pk_url_kwarg = 'post_id'
+
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+        self.handle_visited()
+        return response
+
+    def handle_visited(self):
+        increase_pv = False
+        increase_uv = False
+        uid = self.request.uid
+        pv_key = 'pv:%s:%s' % (uid, self.request.path)
+        uv_key = 'uv:%s:%s:%s' % (uid, str(date.today()), self.request.path)
+        if not cache.get(pv_key):
+            increase_pv = True
+            cache.set(pv_key, 1, 1*60)    #1分钟有效
+
+        if not cache.get(uv_key):
+            increase_uv = True
+            cache.set(pv_key, 1, 24*60*60)
+
+        if increase_pv and increase_uv:
+            Post.objects.filter(pk=self.object.id).update(pv=F('pv') + 1, uv=F('uv')+1)
+        elif increase_pv:
+            Post.objects.filter(pk=self.object.id).update(pv=F('pv') + 1)
+        elif increase_uv:
+            Post.objects.filter(pk=self.object.id).update(uv=F('uv') + 1)
+
+
+
+class SearchView(IndexView):
+    def get_context_data(self):
+        context = super().get_context_data()
+        context.update({
+            'keyword': self.request.GET.get('keyword', '')
+
+        })
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        keyword = self.request.GET.get('keyword')
+        if not keyword:
+            return queryset
+        return queryset.filter(Q(title__icontains=keyword) | Q(desc__icontains=keyword))
+
+
 
 
 
--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -17,15 +17,19 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from custom_site import custom_site
-from blog.views import post_list, post_detail
+from blog.views import post_list
+from blog.views import (IndexView, CategoryView, TagView, PostDetailView)
 from config.views import links
+from blog.views import PostDetailView, SearchView
 
 urlpatterns = [
-    url(r'^$', post_list),
-    url(r'^category/(?P<category_id>\d+)/$', post_list, name='category-list'),
+    url(r'^$', IndexView.as_view, name='index'),
+    url(r'^category/(?P<category_id>\d+)/$', CategoryView.as_view, name='category-list'),
     url(r'^tag/(?P<tag_id>\d+)/$', post_list, name='tag-list'),
-    url(r'^post/(?P<post_id>\d+).html$', post_detail, name='post-detail'),
+    # url(r'^post/(?P<post_id>\d+).html$', PostDetailView.as_view(), name='post-detail'),
+    url(r'^post/(?P<pk>\d+).html$', PostDetailView.as_view(), name='post-detail'),
     url(r'^links/$', links, name='links'),
+    url(r'^search/$', SearchView.as_view(), name='search'),
 
     url(r'^super_admin/', admin.site.urls, name='super-admin'),
     url(r'^admin/', custom_site.urls, name='admin'),
